08d5e390dcacc22dcbadbf606bdd8c9e7e0177ae
RuslanGon <ruslan441006@gmail.com>
2024-09-06 15:31:00 +0300

    wer

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,14 +1,29 @@
+import { NavLink } from 'react-router-dom'
 import logo from '../assets/images/logo.svg'
 import css from '../page/MainPage.module.css'
+import clsx from 'clsx';
+
+const getNavLinkClassName = ({ isActive }) => {
+    return clsx(css.navLink, {
+      [css.active]: isActive,
+    });
+  };
 
 
 const MainPage = () => {
   return (
-    <div>
-    <img className={css.logo} src={logo} alt="logo" />
-
+    <div className={css.header}>
+      <img className={css.logo} src={logo} alt="logo" />
+      <nav className={css.nav}>
+      <NavLink className={getNavLinkClassName} to="/">
+        Home
+      </NavLink>
+      <NavLink className={getNavLinkClassName} to="/">
+        Catalog
+      </NavLink>
+      </nav>
     </div>
-  )
+  );
 }
 
 export default MainPage
