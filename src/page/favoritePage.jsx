565e149aa00082afbf67944aafd1e461bed3acc1
RuslanGon <ruslan441006@gmail.com>
2024-09-07 21:39:06 +0300

    wer

--- src/page/favoritePage.jsx
+++ src/page/favoritePage.jsx
@@ -1,6 +1,16 @@
-
+import { useEffect } from "react";
+import css from "./FavoritePage.module.css"; 
 
 const FavoritePage = () => {
+
+  useEffect(() => {
+    document.body.classList.add(css.pageBackground);
+
+    return () => {
+      document.body.classList.remove(css.pageBackground);
+    };
+  }, []);
+
   return (
     <div>FavoritePage</div>
   )
