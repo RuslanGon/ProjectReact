1af8fb4f4fd1dc0e200b982f2b5f56b68cdc8269
RuslanGon <ruslan441006@gmail.com>
2024-09-07 12:00:42 +0300

    wer

--- src/page/CatalogPage.jsx
+++ src/page/CatalogPage.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const CatalogPage = () => {
-  const [products, setProducts] = useState([]); // Начальное состояние пустого массива
+  const [products, setProducts] = useState([]); 
 
   useEffect(() => {
     async function fetchProducts() {
@@ -11,7 +11,7 @@ const CatalogPage = () => {
           "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers"
         );
         console.log(data);
-        setProducts(data.items); // Сохранение данных в состояние
+        setProducts(data.items); 
       } catch (error) {
         console.error("Error fetching data: ", error);
       }
@@ -21,18 +21,18 @@ const CatalogPage = () => {
 
   return (
     <ul>
-      {products.map((product) => (
+      {Array.isArray(products) && products.map((product) => (
         <li key={product.id}>
           <img src={product.gallery[0]?.thumb} alt={product.name} />
           <h2>{product.name}</h2>
-          <p>{`Price: $${product.price}`}</p>
-          <p>{`Rating: ${product.rating}`}</p>
+          <p>{product.price}</p>
+          <p>{product.rating}</p> 
           <h3>{product.location}</h3>
           <p>{product.description}</p>
-          <p>{product.transmission === "automatic" ? "Automatic" : "Manual"}</p>
-          <p>{product.engine === "petrol" ? "Petrol" : "Diesel"}</p>
-          {product.kitchen && <p>Kitchen</p>}
-          {product.AC && <p>AC</p>}
+          <p>Automatic</p>
+          <p>Petrol</p>
+          <p>Kitchen</p>
+          <p>AC</p>
           <button>Show more</button>
         </li>
       ))}
