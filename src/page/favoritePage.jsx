6bfe876bbb2497c3e5ddd80d4fdc406f41ae1864
RuslanGon <ruslan441006@gmail.com>
2024-09-09 17:08:18 +0300

    wer

--- src/page/favoritePage.jsx
+++ src/page/favoritePage.jsx
@@ -1,15 +1,15 @@
-import { useEffect } from "react";
-import css from "./FavoritePage.module.css"; 
+// import { useEffect } from "react";
+// import css from "./FavoritePage.module.css"; 
 
 const FavoritePage = () => {
 
-  useEffect(() => {
-    document.body.classList.add(css.pageBackground);
+  // useEffect(() => {
+  //   document.body.classList.add(css.pageBackground);
 
-    return () => {
-      document.body.classList.remove(css.pageBackground);
-    };
-  }, []);
+  //   return () => {
+  //     document.body.classList.remove(css.pageBackground);
+  //   };
+  // }, []);
 
   return (
     <div>FavoritePage</div>
