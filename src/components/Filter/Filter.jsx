691068eea7e0f1dc2874fd16511621b48c33ca5c
RuslanGon <ruslan441006@gmail.com>
2024-09-09 16:59:33 +0300

    wer

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -7,6 +7,7 @@ import kap from "../../assets/images/kap.png";
 import bi1 from "../../assets/images/bi1.png";
 import bi2 from "../../assets/images/bi2.png";
 import bi3 from "../../assets/images/bi3.png";
+import { Link } from 'react-router-dom';
 
 const Filter = () => {
   return (
@@ -15,42 +16,58 @@ const Filter = () => {
       <h2 className={css.title}>Vehicle equipment</h2>
       <hr className={css.line} />
       <ul className={css.filterlist}>
-        <li className={css.filteritem}>
-          <img className={css.logo} src={wind} alt="" />
-          <p className={css.logotext}>AC</p>
-        </li>
-        <li className={css.filteritem}>
-          <img className={css.logo} src={cah} alt="" />
-          <p className={css.logotext}>Automatic</p>
-        </li>
-        <li className={css.filteritem}>
-          <img className={css.logo} src={cup} alt="" />
-          <p className={css.logotext}>Kitchen</p>
-        </li>
-        <li className={css.filteritem}>
-          <img className={css.logo} src={tv} alt="" />
-          <p className={css.logotext}>TV</p>
-        </li>
-        <li className={css.filteritem}>
-          <img className={css.logo} src={kap} alt="" />
-          <p className={css.logotext}>Bathroom</p>
-        </li>
+        <Link>
+          <li className={css.filteritem}>
+            <img className={css.logo} src={wind} alt="" />
+            <p className={css.logotext}>AC</p>{" "}
+          </li>
+        </Link>
+        <Link>
+          <li className={css.filteritem}>
+            <img className={css.logo} src={cah} alt="" />
+            <p className={css.logotext}>Automatic</p>
+          </li>
+        </Link>
+        <Link>
+          <li className={css.filteritem}>
+            <img className={css.logo} src={cup} alt="" />
+            <p className={css.logotext}>Kitchen</p>
+          </li>
+        </Link>
+        <Link>
+          <li className={css.filteritem}>
+            <img className={css.logo} src={tv} alt="" />
+            <p className={css.logotext}>TV</p>
+          </li>
+        </Link>
+        <Link>
+          <li className={css.filteritem}>
+            <img className={css.logo} src={kap} alt="" />
+            <p className={css.logotext}>Bathroom</p>
+          </li>
+        </Link>
       </ul>
       <h2 className={css.title}>Vehicle type</h2>
       <hr className={css.line} />
       <ul className={css.filterlist}>
-        <li className={css.filteritem}>
-          <img className={css.logo} src={bi1} alt="" />
-          <p className={css.logotext}>Van</p>
-        </li>
-        <li className={css.filteritem}>
-          <img className={css.logo} src={bi2} alt="" />
-          <p className={css.logotext}>Fully Integrated</p>
-        </li>
-        <li className={css.filteritem}>
-          <img className={css.logo} src={bi3} alt="" />
-          <p className={css.logotext}>Alcove</p>
-        </li>
+        <Link>
+          <li className={css.filteritem}>
+            <img className={css.logo} src={bi1} alt="" />
+            <p className={css.logotext}>Van</p>
+          </li>
+        </Link>
+        <Link>
+          <li className={css.filteritem}>
+            <img className={css.logo} src={bi2} alt="" />
+            <p className={css.logotext}>Fully Integrated</p>
+          </li>
+        </Link>
+        <Link>
+          <li className={css.filteritem}>
+            <img className={css.logo} src={bi3} alt="" />
+            <p className={css.logotext}>Alcove</p>
+          </li>
+        </Link>
       </ul>
     </div>
   );
