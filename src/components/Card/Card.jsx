51160d727c6e28a495c76e2f01125e39517fdc65
RuslanGon <ruslan441006@gmail.com>
2024-09-10 13:21:37 +0300

    wer

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,61 @@
-import { useParams } from "react-router-dom"
-
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Loader from "../Loader/Loader.jsx";
+import Error from "../Error/Error.jsx";
+import { requestProductCardById } from "../../services/api.js";
+import css from '../Card/Card.module.css'
 
 const Card = () => {
+  const { productId } = useParams();
+  const [productDetails, setProductDetails] = useState(null);
+  console.log(productDetails);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-    const {productId} = useParams()
+  useEffect(() => {
+    async function fetchProductDetails() {
+      try {
+        setIsLoading(true);
+        const data = await requestProductCardById(productId);
+        setProductDetails(data);  
+      } catch (error) {
+        console.log(error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchProductDetails();
+  }, [productId]);
 
   return (
-    <div>
-        <h1>Car {productId}</h1>
+    <div className={css.cardContainer}>
+      {isLoading && <Loader />}
+      {isError && <Error />}
+      {productDetails !== null && (
+        <div className={css.card}>
+          <h1>{productDetails.name}</h1>
+          <p>{productDetails.price}</p>
+          <p>{productDetails.rating}</p>
+          <p>{productDetails.location}</p>
+          <p>{productDetails.description}</p>
+          <p>{productDetails.form}</p>
+          {productDetails.gallery && (
+            <div className={css.gallery}>
+              {productDetails.gallery.map((item, index) => (
+                <img
+                  key={index}
+                  src={item.original}
+                  alt={`Gallery image ${index + 1}`}
+                  className={css.galleryImage}
+                />
+              ))}
+            </div>
+          )}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/services/api.js
+++ src/services/api.js
@@ -22,3 +22,11 @@ export const requestProductsByIds = async (ids) => {
   const data = await Promise.all(responses.map(res => res.json()));
   return { items: data };
 };
+
+
+export const requestProductCardById = async (productId) => {
+  const { data } = await axios.get(
+    `https://66b1f8e71ca8ad33d4f5f63e.mockapi.io/campers/${productId}`
+  );
+  return data;
+};
