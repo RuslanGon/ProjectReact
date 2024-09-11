84bb1327352439f1f576de443f0801cd01744cf2
RuslanGon <ruslan441006@gmail.com>
2024-09-11 15:31:07 +0300

    wer

--- src/components/TehCard/TehCard.jsx
+++ src/components/TehCard/TehCard.jsx
@@ -3,30 +3,25 @@ import { useParams } from "react-router-dom";
 import { requestProductCardById } from "../../services/api.js";
 import Loader from "../Loader/Loader.jsx";
 import Error from "../Error/Error.jsx";
-import css from '../../components/TehCard/TehCard.module.css'
+import css from "../../components/TehCard/TehCard.module.css";
 import cah from "../../assets/images/cah.png";
 import oil from "../../assets/images/oil.png";
 import cup from "../../assets/images/cup.png";
 import wind from "../../assets/images/wind.png";
 import radio from "../../assets/images/radio.png";
 
-
-
-
-
 const TehCard = () => {
-
-const { productId } = useParams();
-const [productDetails, setProductDetails] = useState(null);
-const [isLoading, setIsLoading] = useState(false);
-const [isError, setIsError] = useState(false);
+  const { productId } = useParams();
+  const [productDetails, setProductDetails] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     async function fetchProductDetails() {
       try {
         setIsLoading(true);
         const data = await requestProductCardById(productId);
-        setProductDetails(data);  
+        setProductDetails(data);
       } catch (error) {
         console.log(error);
         setIsError(true);
@@ -42,26 +37,26 @@ const [isError, setIsError] = useState(false);
       {isLoading && <Loader />}
       {isError && <Error />}
       <div className={css.divcah1}>
-                  <img className={css.cah} src={cah} alt="" />
-                  <p className={css.auto}>Automatic</p>
-                </div>
-                <div className={css.divcah1}>
-                  <img className={css.oil} src={oil} alt="" />
-                  <p>Petrol</p>
-                </div>
-                <div className={css.divcah1}>
-                  <img className={css.cup} src={cup} alt="" />
-                  <p>Kitchen</p>
-                </div>
-                <br />
-                <div className={css.divcah1}>
-                  <img className={css.wind} src={wind} alt="" />
-                  <p>AC</p>
-                </div>
-                <div className={css.divcah1}>
-                  <img className={css.radio} src={radio} alt="" />
-                  <p>Radio</p>
-                </div>
+        <img className={css.cah} src={cah} alt="" />
+        <p className={css.auto}>Automatic</p>
+      </div>
+      <div className={css.divcah1}>
+        <img className={css.oil} src={oil} alt="" />
+        <p>Petrol</p>
+      </div>
+      <div className={css.divcah1}>
+        <img className={css.cup} src={cup} alt="" />
+        <p>Kitchen</p>
+      </div>
+      <br />
+      <div className={css.divcah1}>
+        <img className={css.wind} src={wind} alt="" />
+        <p>AC</p>
+      </div>
+      <div className={css.divcah1}>
+        <img className={css.radio} src={radio} alt="" />
+        <p>Radio</p>
+      </div>
       <h2 className={css.title}>Vehicle details</h2>
       <hr className={css.line} />
       {productDetails !== null && (
@@ -94,6 +89,6 @@ const [isError, setIsError] = useState(false);
       )}
     </div>
   );
-}
+};
 
-export default TehCard
+export default TehCard;
